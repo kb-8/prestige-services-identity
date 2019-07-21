10915bdceb070690e4e476f0315bd945cc087cf6
ivavivanova <ivavivanova@outlook.com>
2019-07-22 00:01:36 +0300

    Merge Common and Infrastructure projects; Add password hasher

--- src/Prestige.Identity.Data/Migrations/20190721205459_AddPasswordHashColumn.Designer.cs
+++ src/Prestige.Identity.Data/Migrations/20190721205459_AddPasswordHashColumn.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Prestige.Identity.Data.DAL;
 
 namespace Prestige.Identity.Data.Migrations
 {
     [DbContext(typeof(IdentityDbContext))]
-    partial class IdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190721205459_AddPasswordHashColumn")]
+    partial class AddPasswordHashColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
